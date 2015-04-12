5d96bcec31eb41c3a8d2411b38d77d0221a224f7
unknown <s.dilip.kumar@BDC8-L-5079371.GDNINDIA.COM>
2015-04-12 22:00:46 +0530

    fixed filterData

--- src/Deviab/AppBundle/Resources/public/js/app/app.js
+++ src/Deviab/AppBundle/Resources/public/js/app/app.js
@@ -3,21 +3,62 @@ app.config(function($routeProvider, $interpolateProvider) {
     $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
 });
 
-app.controller('filterController', function($scope) {
-    $scope.states = [
+app.controller('filterController', function($http) {
+
+
+
+    this.filterData = {
+        selectedStates: ['cg'],
+        selectedAmountNeeded: ['cg'],
+        selectedGender: ['cg'],
+        selectedEducation: ['cg'],
+        selectedAge: ['cg'],
+
+    };
+
+    var res = $http.post('/search', dataObj);
+        res.success(function(data, status, headers, config) {
+            $scope.message = data;
+        });
+        res
+    
+    this.states = [
         'cg',
         'tn',
         'kr',
         'mh'
     ];
-    $scope.filterData = {
-        selectedStates: ['cg']
-    };
+    this.amountNeeded = [
+        'cg',
+        'tn',
+        'kr',
+        'mh'
+    ];
+    this.gender = [
+        'cg',
+        'tn',
+        'kr',
+        'mh'
+    ];
+    this.age = [
+        'cg',
+        'tn',
+        'kr',
+        'mh'
+    ];
+    this.education= [
+        'cg',
+        'tn',
+        'kr',
+        'mh'
+    ];
+
+
     $scope.checkAll = function() {
-        $scope.filterData.selectedStates = angular.copy($scope.states);
-    };
+        this.filterData.selectedStates = angular.copy(this.states);
+            };
     $scope.uncheckAll = function() {
-        $scope.filterData.selectedStates = [];
+        this.filterData.selectedStates = [];
     };
 
 });
