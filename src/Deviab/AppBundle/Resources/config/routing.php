a0d7f1dcf16bfc41ef9ec984e207b4e2dd3ddcfc
nitinbhojwani <nitinbhojwani@outlook.com>
2015-07-11 00:17:59 +0530

    Adding css, home page, removing unnecessary files

--- src/Deviab/AppBundle/Resources/config/routing.php
+++ src/Deviab/AppBundle/Resources/config/routing.php
@@ -5,7 +5,7 @@ use Symfony\Component\Routing\Route;
 
 $collection = new RouteCollection();
 
-$collection->add('deviab_app_homepage', new Route('/hello/{name}', array(
+$collection->add('deviab_app_homepage', new Route('/', array(
     '_controller' => 'DeviabAppBundle:Default:index',
 )));
 
