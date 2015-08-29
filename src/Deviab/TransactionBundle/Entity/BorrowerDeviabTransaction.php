bf56b3a4e86499a5d70e23175d9e4fe62af1fd52
nitinbhojwani <nitinbhojwani@outlook.com>
2015-08-30 00:57:08 +0530

    transactional model added

---
+++ src/Deviab/TransactionBundle/Entity/BorrowerDeviabTransaction.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Deviab\TransactionBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * BorrowerDeviabTransaction
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class BorrowerDeviabTransaction
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Lender", inversedBy="borrowerDeviabTransactions")
+     */
+    private $lender;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Borrower", inversedBy="deviabBorrowerTransactions")
+     */
+    private $borrower;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="timestamp", type="datetime")
+     */
+    private $timestamp;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="amount", type="float")
+     */
+    private $amount;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set timestamp
+     *
+     * @param \DateTime $timestamp
+     * @return BorrowerDeviabTransaction
+     */
+    public function setTimestamp($timestamp)
+    {
+        $this->timestamp = $timestamp;
+
+        return $this;
+    }
+
+    /**
+     * Get timestamp
+     *
+     * @return \DateTime 
+     */
+    public function getTimestamp()
+    {
+        return $this->timestamp;
+    }
+
+    /**
+     * Set amount
+     *
+     * @param float $amount
+     * @return BorrowerDeviabTransaction
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    /**
+     * Get amount
+     *
+     * @return float 
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+}
