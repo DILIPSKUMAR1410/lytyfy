c3330351fdd7c46ea9d79428dcd83224492a1f58
nitinbhojwani <nitinbhojwani@outlook.com>
2015-09-06 02:34:32 +0530

    ajax for borrower api removal

--- src/Deviab/FabricBundle/Resources/public/js/app/portfolio/controller/portfolio.js
+++ src/Deviab/FabricBundle/Resources/public/js/app/portfolio/controller/portfolio.js
@@ -1,10 +1,10 @@
 app.controller("portfolioController", function($scope,$http) {
     $scope.jsonResponce = null;
-    var res = $http.get('/borrowers/2.json');
-    res.success(function(data, status, headers, config) {
-      $scope.portfolioResponce = data;
-      console.log(data);
-  });
+  //   var res = $http.get('/borrowers/2.json');
+  //   res.success(function(data, status, headers, config) {
+  //     $scope.portfolioResponce = data;
+  //     console.log(data);
+  // });
   // res.error(function(data, status, headers, config) {
   //   alert( "failure message: " + JSON.stringify({data: data}));
   // });
