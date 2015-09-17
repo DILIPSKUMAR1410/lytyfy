1cba3884309b18ab8b4dc6db4055bb99e693842f
Nitin Bhojwani <nitinbhojwani1993@gmail.com>
2015-09-17 20:17:02 +0530

    Sao's FabricBundle added & AppBundle removed

---
+++ src/Deviab/FabricBundle/Controller/ProjectController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Deviab\FabricBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ProjectController extends Controller
+{
+	public function projectAction()
+    {
+        return $this->render('FabricBundle:Project:project.html.twig');
+    }
+}
---
+++ src/Deviab/FabricBundle/Resources/public/js/app/portfolio/controller/Profile.js
@@ -0,0 +1,4 @@
+app.controller("ProfileController", function($scope,$http) {
+$scope.amounts = "raghav";
+    
+});
