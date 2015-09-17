2ad29d0e2e68c475f27a36c43835b6380410b483
Raghav_Sao <saoraghavendra@gmail.com>
2015-09-17 19:38:36 +0530

    signin/signup ui created

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Deviab\AppBundle\DeviabAppBundle(),
             new Deviab\BorrowerBundle\DeviabBorrowerBundle(),
             new Deviab\DatabaseBundle\DeviabDatabaseBundle(),
             new FOS\RestBundle\FOSRestBundle(),
