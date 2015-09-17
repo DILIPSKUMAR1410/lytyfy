9abe5c4ade2b3e6f6cd22be930af1d9e5d935ab2
Nitin Bhojwani <nitinbhojwani1993@gmail.com>
2015-09-17 20:48:37 +0530

    Login merged and conflicts resolved

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
             new Deviab\FabricBundle\FabricBundle(),
             new Deviab\TransactionBundle\DeviabTransactionBundle(),
             new Deviab\RepaymentBundle\DeviabRepaymentBundle(),
+            new FOS\UserBundle\FOSUserBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
