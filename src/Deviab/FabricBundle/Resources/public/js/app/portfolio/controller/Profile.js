4f14a54ba27facf6b3bb9d342aead18cd9e45815
Raghav_Sao <saoraghavendra@gmail.com>
2015-09-27 21:21:49 +0530

    loader gif added

--- src/Deviab/FabricBundle/Resources/public/js/app/portfolio/controller/Profile.js
+++ src/Deviab/FabricBundle/Resources/public/js/app/portfolio/controller/Profile.js
@@ -6,7 +6,7 @@ app.controller("ProfileController", function ($scope, $http) {
         $scope.profileResponce = data;
        var link = $scope.profileResponce.to_lender_transactions[0].project.to_project_lender_transactions[0];
       $scope.withdraw = link.amount - link.lender.current_status.pricipal_left - link.lender.wallet.credits;
-      
+      $(".loading").hide();
     });
     res.error(function(data, status, headers, config) {
       console.log( "failure message: " + JSON.stringify({data: data}));
