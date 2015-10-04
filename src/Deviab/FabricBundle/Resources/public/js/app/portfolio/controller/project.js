aa2e815c6b94f25e63ceb500e18e9d9bcbbcdd60
Raghav_Sao <saoraghavendra@gmail.com>
2015-10-05 03:45:00 +0530

    nav zindex changed

--- src/Deviab/FabricBundle/Resources/public/js/app/portfolio/controller/project.js
+++ src/Deviab/FabricBundle/Resources/public/js/app/portfolio/controller/project.js
@@ -1,12 +1,12 @@
 app.controller("ProjectController", function ($scope, $http) {
     $scope.raised = null;
     $scope.projectResponce = null;
-    // var res = $http.get('/api/v1/projects/1.json');
-    // res.success(function (data, status, headers, config) {
-    //     $scope.projectResponce = data;
-    //     $scope.raised = ($scope.projectResponce.quantum/120000*100).toFixed(2);
-    // });
-    // res.error(function(data, status, headers, config) {
-    //   console.log( "failure message: " + JSON.stringify({data: data}));
-    // });
+    var res = $http.get('/api/v1/projects/1.json');
+    res.success(function (data, status, headers, config) {
+        $scope.projectResponce = data;
+        $scope.raised = ($scope.projectResponce.quantum/120000*100).toFixed(2);
+    });
+    res.error(function(data, status, headers, config) {
+      console.log( "failure message: " + JSON.stringify({data: data}));
+    });
 });
