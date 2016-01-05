b71f720b14ada5f96610bc2bf3c012fd9e2355fd
nehagoswami2403 <nehagoswami2403@gmail.com>
2016-01-05 19:12:19 +0530

    payu webhook test commit

--- src/Deviab/TransactionBundle/Services/InvestmentService.php
+++ src/Deviab/TransactionBundle/Services/InvestmentService.php
@@ -9,8 +9,10 @@
 namespace Deviab\TransactionBundle\Services;
 
 use Deviab\TransactionBundle\Entity\DeviabBorrowerTransaction;
+use Deviab\TransactionBundle\Entity\DeviabLenderTransaction;
 use Deviab\TransactionBundle\Entity\LenderDeviabTransaction;
 use Doctrine\Bundle\DoctrineBundle\Registry as Doctrine;
+use Doctrine\ORM\Query\ResultSetMapping;
 use FOS\RestBundle\View\View;
 use FOS\RestBundle\Util\Codes;
 use Deviab\BorrowerBundle\Services\BaseService;
@@ -21,14 +23,14 @@ class InvestmentService extends BaseService
     /**
      * @param Doctrine $doctrine
      */
-    public function __construct(Doctrine $doctrine)
+    public function __construct( Doctrine $doctrine )
     {
         parent::__construct($doctrine);
 
         $this->doctrine = $doctrine;
     }
 
-    public function getLenderInvestment($lenderId)
+    public function getLenderInvestment( $lenderId )
     {
         $lenderRepository = $this->doctrine->getRepository('DeviabDatabaseBundle:LenderDetails');
         $lender = $lenderRepository->find($lenderId);
@@ -90,7 +92,7 @@ class InvestmentService extends BaseService
         return View::create("something went wrong dude", Codes::HTTP_BAD_REQUEST);
     }
 
-    public function getEMR($EntitycurrentStatus)
+    public function getEMR( $EntitycurrentStatus )
     {
         $pl = $EntitycurrentStatus->getPricipalLeft();
         $il = $EntitycurrentStatus->getInterestLeft();
@@ -103,7 +105,7 @@ class InvestmentService extends BaseService
      * @param $lenderId
      * @return View
      */
-    public function getWalletSummary($lenderId)
+    public function getWalletSummary( $lenderId )
     {
         $lenderRepository = $this->doctrine->getRepository('DeviabDatabaseBundle:LenderDetails');
         $lender = $lenderRepository->find($lenderId);
@@ -118,5 +120,44 @@ class InvestmentService extends BaseService
 
     }
 
+    public function walletWithdrawl( User $user )
+    {
+        $lender = $user->getlender();
+        $transaction = new DeviabLenderTransaction();
+        $transaction->setLender($lender);
+
+        $dateTime = date("Y-m-d H:i:s");
+        $transaction->setTimestamp($dateTime);
+        $txnid = uniqid($user->getEmail());
+        $lenderId = $lender->getId();
+        $balance = $lender->getWallet()->getCredits();
+        $status = "under process";
+        $remarks = "credited in your account";
+        if ($balance >= 2000) {
+            $rsm = new ResultSetMapping();
+
+            $query = $this->em->createNativeQuery('INSERT INTO deviab_lender_transactions SET lender_id = ? ,transactionId = ?,project_id = ?,status = ?,remarks = ?', $rsm);
+            $query->setParameter(1, $lenderId);
+            $query->setParameter(2, $txnid);
+            $query->setParameter(3, $balance);
+            $query->setParameter(4, 1);
+            $query->setParameter(5, $status);
+            $query->setParameter(6, $remarks);
+            $query->setParameter(7, $dateTime);
+
+            $result = $query->_doExecute();
+            $update_query = $this->em->createNativeQuery('UPDATE lender_wallet SET credits = ? where lender_id = ?', $rsm);
+            $update_query->setParameter(1, 0);
+            $update_query->setParameter(2, $lenderId);
+
+            $update_result = $update_query->_doExecute();
+
+            return View::create("ok", Codes::HTTP_OK);
+        } else {
+            return View::create("you don't have enough money in your wallet", Codes::HTTP_BAD_REQUEST);
+        }
+    }
 
 }
+
+
